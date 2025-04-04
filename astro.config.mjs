a67daf928dc8fa16062d63bddaddd72760a4a9a8
mgramigna <matthewgramigna@gmail.com>
2025-04-03 20:04:21 -0400

    feat: add cd

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://abstractdevs.github.io",
+  base: "librarian",
   vite: {
     plugins: [tailwindcss()],
   },
