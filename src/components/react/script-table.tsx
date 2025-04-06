481df3e550c6172d1db0e5601e70a5ce46307975
mgramigna <matthewgramigna@gmail.com>
2025-04-06 14:28:26 -0400

    chore: make character count a tooltip

--- src/components/react/script-table.tsx
+++ src/components/react/script-table.tsx
@@ -1,3 +1,4 @@
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
 import {
   Table,
   TableBody,
@@ -26,14 +27,28 @@ export const ScriptTable = ({ scripts }: { scripts: Script[] }) => {
         {scripts.map(({ id, author, name, characterBreakdown, scriptJsonString }) => (
           <TableRow
             key={id}
-            onClick={() => (window.location.href = `/scripts/${id}`)}
+            onClick={() => {
+              window.location.href = `/scripts/${id}`;
+            }}
             className="cursor-pointer"
           >
             <TableCell>{name}</TableCell>
             <TableCell>{author}</TableCell>
             <TableCell>
-              {characterBreakdown.townsfolkCount} / {characterBreakdown.outsiderCount} /{" "}
-              {characterBreakdown.minionCount} / {characterBreakdown.demonCount}
+              <TooltipProvider>
+                <Tooltip>
+                  <TooltipTrigger>
+                    {characterBreakdown.townsfolkCount} / {characterBreakdown.outsiderCount} /{" "}
+                    {characterBreakdown.minionCount} / {characterBreakdown.demonCount}
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <p>{characterBreakdown.townsfolkCount} Townsfolk</p>
+                    <p>{characterBreakdown.outsiderCount} Outsiders</p>
+                    <p>{characterBreakdown.minionCount} Minions</p>
+                    <p>{characterBreakdown.demonCount} Demons</p>
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
             </TableCell>
             <TableCell className="flex items-center gap-2">
               <CopyScriptJsonButton jsonString={scriptJsonString} size="icon" />
