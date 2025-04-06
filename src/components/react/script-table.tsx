6823786fccfe877ce4daea72361f0bc6c5a0bcd4
mgramigna <matthewgramigna@gmail.com>
2025-04-06 15:09:44 -0400

    feat: basic table querying

--- src/components/react/script-table.tsx
+++ src/components/react/script-table.tsx
@@ -11,52 +11,79 @@ import {
 import type { Script } from "@/utils/script";
 import { CopyScriptJsonButton } from "./copy-script-json-button";
 import { DownloadScriptJsonButton } from "./download-script-json-button";
+import { useState } from "react";
+import { Input } from "../ui/input";
+import { SearchIcon } from "lucide-react";
+import { cn } from "@/utils/cn";
 
 export const ScriptTable = ({ scripts }: { scripts: Script[] }) => {
+  const [query, setQuery] = useState("");
+
+  const filteredScripts = scripts.filter((script) => {
+    return (
+      script.name.toLowerCase().includes(query.toLowerCase()) ||
+      script.author.toLowerCase().includes(query.toLowerCase())
+    );
+  });
+
   return (
-    <Table>
-      <TableHeader>
-        <TableRow>
-          <TableHead>Script</TableHead>
-          <TableHead>Author</TableHead>
-          <TableHead>Characters</TableHead>
-          <TableHead>Quick Actions</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {scripts.map(({ id, author, name, characterBreakdown, scriptJsonString }) => (
-          <TableRow
-            key={id}
-            onClick={() => {
-              window.location.href = `/scripts/${id}`;
-            }}
-            className="cursor-pointer"
-          >
-            <TableCell>{name}</TableCell>
-            <TableCell>{author}</TableCell>
-            <TableCell>
-              <TooltipProvider>
-                <Tooltip>
-                  <TooltipTrigger>
-                    {characterBreakdown.townsfolkCount} / {characterBreakdown.outsiderCount} /{" "}
-                    {characterBreakdown.minionCount} / {characterBreakdown.demonCount}
-                  </TooltipTrigger>
-                  <TooltipContent>
-                    <p>{characterBreakdown.townsfolkCount} Townsfolk</p>
-                    <p>{characterBreakdown.outsiderCount} Outsiders</p>
-                    <p>{characterBreakdown.minionCount} Minions</p>
-                    <p>{characterBreakdown.demonCount} Demons</p>
-                  </TooltipContent>
-                </Tooltip>
-              </TooltipProvider>
-            </TableCell>
-            <TableCell className="flex items-center gap-2">
-              <CopyScriptJsonButton jsonString={scriptJsonString} size="icon" />
-              <DownloadScriptJsonButton jsonString={scriptJsonString} scriptId={id} size="icon" />
-            </TableCell>
+    <div className="space-y-4">
+      <div className="relative flex items-center">
+        <SearchIcon
+          className={cn("absolute left-3 h-4 w-4 shrink-0 opacity-50", query !== "" && "hidden")}
+        />
+        <Input
+          type="text"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          placeholder="Search by name, author"
+          className="placeholder:pl-5"
+        />
+      </div>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Script</TableHead>
+            <TableHead>Author</TableHead>
+            <TableHead>Characters</TableHead>
+            <TableHead>Quick Actions</TableHead>
           </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+        </TableHeader>
+        <TableBody>
+          {filteredScripts.map(({ id, author, name, characterBreakdown, scriptJsonString }) => (
+            <TableRow
+              key={id}
+              onClick={() => {
+                window.location.href = `/scripts/${id}`;
+              }}
+              className="cursor-pointer"
+            >
+              <TableCell>{name}</TableCell>
+              <TableCell>{author}</TableCell>
+              <TableCell>
+                <TooltipProvider>
+                  <Tooltip>
+                    <TooltipTrigger>
+                      {characterBreakdown.townsfolkCount} / {characterBreakdown.outsiderCount} /{" "}
+                      {characterBreakdown.minionCount} / {characterBreakdown.demonCount}
+                    </TooltipTrigger>
+                    <TooltipContent>
+                      <p>{characterBreakdown.townsfolkCount} Townsfolk</p>
+                      <p>{characterBreakdown.outsiderCount} Outsiders</p>
+                      <p>{characterBreakdown.minionCount} Minions</p>
+                      <p>{characterBreakdown.demonCount} Demons</p>
+                    </TooltipContent>
+                  </Tooltip>
+                </TooltipProvider>
+              </TableCell>
+              <TableCell className="flex items-center gap-2">
+                <CopyScriptJsonButton jsonString={scriptJsonString} size="icon" />
+                <DownloadScriptJsonButton jsonString={scriptJsonString} scriptId={id} size="icon" />
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
   );
 };
