6eb4a8426f425d79affa0b4383055233e03b55b7
mgramigna <matthewgramigna@gmail.com>
2025-04-06 17:27:34 -0400

    refactor: move filters to separate component

---
+++ src/components/react/script-table/filters.tsx
@@ -0,0 +1,141 @@
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+
+import { Input } from "../../ui/input";
+import { Check, SearchIcon, UserIcon, XIcon } from "lucide-react";
+import { cn } from "@/utils/cn";
+import { Button } from "@/components/ui/button";
+import { characterRegistry, type CharacterName } from "@/data/characters/registry";
+
+export type TableFilter = {
+  selectedCharacters: CharacterName[];
+  filterText: string;
+};
+
+const characterEntries = Object.entries(characterRegistry);
+
+const townsfolkOptions = characterEntries.filter(
+  ([_, character]) => character.type === "townsfolk",
+);
+
+const outsiderOptions = characterEntries.filter(([_, character]) => character.type === "outsider");
+
+const minionOptions = characterEntries.filter(([_, character]) => character.type === "minion");
+
+const demonOptions = characterEntries.filter(([_, character]) => character.type === "demon");
+
+const characterAutocompleteOptions = [
+  { label: "Townsfolk", options: townsfolkOptions },
+  { label: "Outsiders", options: outsiderOptions },
+  { label: "Minions", options: minionOptions },
+  { label: "Demons", options: demonOptions },
+];
+
+export const Filters = ({
+  globalFilter,
+  setGlobalFilter,
+}: {
+  globalFilter: TableFilter;
+  setGlobalFilter: React.Dispatch<React.SetStateAction<TableFilter>>;
+}) => {
+  return (
+    <div className="flex w-full flex-col items-center gap-2 md:flex-row">
+      <div className="relative flex w-full flex-1 items-center md:w-auto">
+        <SearchIcon
+          className={cn(
+            "absolute left-3 h-4 w-4 shrink-0 opacity-50",
+            globalFilter.filterText !== "" && "hidden",
+          )}
+        />
+        <Input
+          type="text"
+          onChange={(event) =>
+            setGlobalFilter({
+              ...globalFilter,
+              filterText: String(event.target.value),
+            })
+          }
+          placeholder="Search by name, author"
+          className="placeholder:pl-5"
+        />
+      </div>
+      <div className="flex w-full items-center gap-0.5 md:w-auto">
+        <Popover>
+          <PopoverTrigger asChild>
+            <Button variant="outline" role="combobox" className="flex-1 md:flex-none">
+              <UserIcon />
+              {globalFilter.selectedCharacters.length > 0
+                ? `${globalFilter.selectedCharacters.length} Character${globalFilter.selectedCharacters.length > 1 ? "s" : ""}`
+                : "Filter by Character"}
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className="p-0">
+            <Command>
+              <CommandInput placeholder="Search characters..." />
+              <CommandList>
+                <CommandEmpty>No characters found</CommandEmpty>
+                {characterAutocompleteOptions.map(({ label, options }) => (
+                  <CommandGroup key={label} heading={label}>
+                    {options.map(([key, character]) => (
+                      <CommandItem
+                        key={key}
+                        value={key}
+                        onSelect={() => {
+                          if (!globalFilter.selectedCharacters.includes(key as CharacterName)) {
+                            setGlobalFilter((prev) => ({
+                              ...prev,
+                              selectedCharacters: [
+                                ...prev.selectedCharacters,
+                                key as CharacterName,
+                              ],
+                            }));
+                          } else {
+                            setGlobalFilter((prev) => ({
+                              ...globalFilter,
+                              selectedCharacters: prev.selectedCharacters.filter(
+                                (character) => character !== key,
+                              ),
+                            }));
+                          }
+                        }}
+                      >
+                        {character.displayName}
+                        <Check
+                          className={cn(
+                            "ml-auto",
+                            globalFilter.selectedCharacters.includes(key as CharacterName)
+                              ? "opacity-100"
+                              : "opacity-0",
+                          )}
+                        />
+                      </CommandItem>
+                    ))}
+                  </CommandGroup>
+                ))}
+              </CommandList>
+            </Command>
+          </PopoverContent>
+        </Popover>
+        <Button
+          size="icon"
+          variant="ghost"
+          onClick={() =>
+            setGlobalFilter({
+              ...globalFilter,
+              selectedCharacters: [],
+            })
+          }
+        >
+          <XIcon />
+        </Button>
+      </div>
+    </div>
+  );
+};
--- src/components/react/script-table/script-table.tsx
+++ src/components/react/script-table/script-table.tsx
@@ -6,21 +6,9 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import {
-  Command,
-  CommandEmpty,
-  CommandGroup,
-  CommandInput,
-  CommandItem,
-  CommandList,
-} from "@/components/ui/command";
-import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 
 import type { Script } from "@/types/script";
 import { useState } from "react";
-import { Input } from "../../ui/input";
-import { Check, SearchIcon, UserIcon, XIcon } from "lucide-react";
-import { cn } from "@/utils/cn";
 import {
   flexRender,
   getCoreRowModel,
@@ -29,33 +17,10 @@ import {
 } from "@tanstack/react-table";
 import { columns } from "./columns";
 import { Typography } from "../typography";
-import { Button } from "@/components/ui/button";
-import { characterRegistry, type CharacterName } from "@/data/characters/registry";
-
-const characterEntries = Object.entries(characterRegistry);
-
-const townsfolkOptions = characterEntries.filter(
-  ([_, character]) => character.type === "townsfolk",
-);
-
-const outsiderOptions = characterEntries.filter(([_, character]) => character.type === "outsider");
-
-const minionOptions = characterEntries.filter(([_, character]) => character.type === "minion");
-
-const demonOptions = characterEntries.filter(([_, character]) => character.type === "demon");
-
-const characterAutocompleteOptions = [
-  { label: "Townsfolk", options: townsfolkOptions },
-  { label: "Outsiders", options: outsiderOptions },
-  { label: "Minions", options: minionOptions },
-  { label: "Demons", options: demonOptions },
-];
+import { Filters, type TableFilter } from "./filters";
 
 export const ScriptTable = ({ scripts }: { scripts: Script[] }) => {
-  const [globalFilter, setGlobalFilter] = useState<{
-    selectedCharacters: CharacterName[];
-    filterText: string;
-  }>({
+  const [globalFilter, setGlobalFilter] = useState<TableFilter>({
     selectedCharacters: [],
     filterText: "",
   });
@@ -69,7 +34,8 @@ export const ScriptTable = ({ scripts }: { scripts: Script[] }) => {
     state: {
       globalFilter,
     },
-    globalFilterFn: (row, _, { filterText, selectedCharacters }: typeof globalFilter) => {
+    // This is kinda hacky, but it works.
+    globalFilterFn: (row, _, { filterText, selectedCharacters }: TableFilter) => {
       const name = row.getValue("name") as string;
       const author = row.getValue("author") as string;
 
@@ -88,97 +54,7 @@ export const ScriptTable = ({ scripts }: { scripts: Script[] }) => {
 
   return (
     <div className="space-y-4">
-      <div className="flex items-center gap-2">
-        <div className="relative flex flex-1 items-center">
-          <SearchIcon
-            className={cn(
-              "absolute left-3 h-4 w-4 shrink-0 opacity-50",
-              globalFilter.filterText !== "" && "hidden",
-            )}
-          />
-          <Input
-            type="text"
-            onChange={(event) =>
-              setGlobalFilter({
-                ...globalFilter,
-                filterText: String(event.target.value),
-              })
-            }
-            placeholder="Search by name, author"
-            className="placeholder:pl-5"
-          />
-        </div>
-        <div className="flex items-center gap-0.5">
-          <Popover>
-            <PopoverTrigger asChild>
-              <Button variant="outline" role="combobox" className="justify-between">
-                <UserIcon />
-                {globalFilter.selectedCharacters.length > 0
-                  ? `${globalFilter.selectedCharacters.length} Character${globalFilter.selectedCharacters.length > 1 ? "s" : ""}`
-                  : "Filter by Character"}
-              </Button>
-            </PopoverTrigger>
-            <PopoverContent className="p-0">
-              <Command>
-                <CommandInput placeholder="Search characters..." />
-                <CommandList>
-                  <CommandEmpty>No characters found</CommandEmpty>
-                  {characterAutocompleteOptions.map(({ label, options }) => (
-                    <CommandGroup key={label} heading={label}>
-                      {options.map(([key, character]) => (
-                        <CommandItem
-                          key={key}
-                          value={key}
-                          onSelect={() => {
-                            if (!globalFilter.selectedCharacters.includes(key as CharacterName)) {
-                              setGlobalFilter((prev) => ({
-                                ...prev,
-                                selectedCharacters: [
-                                  ...prev.selectedCharacters,
-                                  key as CharacterName,
-                                ],
-                              }));
-                            } else {
-                              setGlobalFilter((prev) => ({
-                                ...globalFilter,
-                                selectedCharacters: prev.selectedCharacters.filter(
-                                  (character) => character !== key,
-                                ),
-                              }));
-                            }
-                          }}
-                        >
-                          {character.displayName}
-                          <Check
-                            className={cn(
-                              "ml-auto",
-                              globalFilter.selectedCharacters.includes(key as CharacterName)
-                                ? "opacity-100"
-                                : "opacity-0",
-                            )}
-                          />
-                        </CommandItem>
-                      ))}
-                    </CommandGroup>
-                  ))}
-                </CommandList>
-              </Command>
-            </PopoverContent>
-          </Popover>
-          <Button
-            size="icon"
-            variant="ghost"
-            onClick={() =>
-              setGlobalFilter({
-                ...globalFilter,
-                selectedCharacters: [],
-              })
-            }
-          >
-            <XIcon />
-          </Button>
-        </div>
-      </div>
+      <Filters globalFilter={globalFilter} setGlobalFilter={setGlobalFilter} />
       <Table>
         <TableHeader>
           {table.getHeaderGroups().map((headerGroup) => (
